a90c54cada23aae30ea909011a77e8c0f552c77a
bernamy <129672420+bernamy@users.noreply.github.com>
2023-04-02 11:43:17 -0300

    Add files via upload

---
+++ Ph001a.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+		<title>Ph001</title>
+		<link rel="shortcut icon" href="favicon.ico" />
+		<meta charset="iso-8859-1"/>
+		<meta name="author" content="Bernardo Meyer">
+		<meta name="description" content="Hello World de PHP">
+		<meta name="keywords" content="sites, web, desenvolvimento, HTML5">
+</head>
+<body>
+	<?php
+		echo "<b>Oi</b>"; 
+	?>
+</body>
+</html>
+
